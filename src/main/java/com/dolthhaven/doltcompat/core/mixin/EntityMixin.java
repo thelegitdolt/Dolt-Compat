88a084944a9ee33ea9d144f8dfe30a901a520741
TheLegitDolt <asphaltpotatofy@gmail.com>
2023-05-07 19:07:06 -0700

    1.1.0 done i think
    (now i will actually work on verdure now when i code)

--- src/main/java/com/dolthhaven/doltcompat/core/mixin/EntityMixin.java
+++ src/main/java/com/dolthhaven/doltcompat/core/mixin/EntityMixin.java
@@ -12,12 +12,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Entity.class)
 public class EntityMixin {
-
     @Inject(at = @At("RETURN"), method = "isInWaterOrRain", cancellable = true)
-    private void isWet(CallbackInfoReturnable<Boolean> cir) {
+    private void DoltCompat$YouAreNowWetInMilk(CallbackInfoReturnable<Boolean> cir) {
         Entity entity = (Entity) ((Object) this);
         BlockState state = entity.level.getBlockState(entity.blockPosition());
         if (state.is(NeapolitanBlocks.MILK_CAULDRON.get()) && state.getValue(LayeredCauldronBlock.LEVEL) > 0 && ModList.get().isLoaded("allurement"))
             cir.setReturnValue(true);
     }
+
 }
