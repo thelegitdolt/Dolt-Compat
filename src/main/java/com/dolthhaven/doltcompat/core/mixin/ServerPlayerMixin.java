136b310465b410f6d28a1ea898527a472cf51233
TheLegitDolt <asphaltpotatofy@gmail.com>
2023-05-07 20:08:07 -0700

    Found a better way to do it

--- src/main/java/com/dolthhaven/doltcompat/core/mixin/ServerPlayerMixin.java
+++ src/main/java/com/dolthhaven/doltcompat/core/mixin/ServerPlayerMixin.java
@@ -4,6 +4,7 @@ import com.dolthhaven.doltcompat.core.DoltCompatConfig;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.monster.Monster;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,9 +12,14 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(ServerPlayer.class)
 public class ServerPlayerMixin {
-    @Inject(method = "Lnet/minecraft/server/level/ServerPlayer;bedInRange(Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;)Z",
+    @Inject(method = "bedInRange(Lnet/minecraft/core/BlockPos;Lnet/minecraft/core/Direction;)Z",
     at = @At("RETURN"), cancellable = true)
     private void DoltCompat$NoMoreTooFarAwayBS(BlockPos pos, Direction dir, CallbackInfoReturnable<Boolean> cir) {
         if (DoltCompatConfig.Common.COMMON.ReliableSleep.get()) cir.setReturnValue(true);
+    }
+
+    @Inject(method = "lambda$startSleepInBed$7(Lnet/minecraft/world/entity/monster/Monster;)Z", at = @At("RETURN"), cancellable = true, remap = false)
+    private void DoltCompat$NoMoreMonsterNearbyBS(Monster monster, CallbackInfoReturnable<Boolean> cir) {
+        if (DoltCompatConfig.Common.COMMON.ReliableSleep.get()) cir.setReturnValue(false);
     }
 }
